c128608739f0a2c624cbecb8b41df5af010d7a20
thecml <chr1000@gmail.com>
2021-05-28 15:30:05 +0200

    reduced code, added argparse

--- folder2lmdb.py
+++ folder2lmdb.py
@@ -62,53 +62,6 @@ class ImageFolderLMDB(data.Dataset):
     def __repr__(self):
         return self.__class__.__name__ + ' (' + self.db_path + ')'
 
-class ImageFolderLMDB_old(data.Dataset):
-    def __init__(self, db_path, transform=None, target_transform=None):
-        import lmdb
-        self.db_path = db_path
-        self.env = lmdb.open(db_path, subdir=osp.isdir(db_path),
-                             readonly=True, lock=False,
-                             readahead=False, meminit=False)
-        with self.env.begin(write=False) as txn:
-            self.length = txn.stat()['entries'] - 1
-            self.keys = msgpack.loads(txn.get(b'__keys__'))
-        # cache_file = '_cache_' + db_path.replace('/', '_')
-        # if os.path.isfile(cache_file):
-        #     self.keys = pickle.load(open(cache_file, "rb"))
-        # else:
-        #     with self.env.begin(write=False) as txn:
-        #         self.keys = [key for key, _ in txn.cursor()]
-        #     pickle.dump(self.keys, open(cache_file, "wb"))
-        self.transform = transform
-        self.target_transform = target_transform
-
-    def __getitem__(self, index):
-        img, target = None, None
-        env = self.env
-        with env.begin(write=False) as txn:
-            byteflow = txn.get(self.keys[index])
-        unpacked = msgpack.loads(byteflow)
-        imgbuf = unpacked[0][b'data']
-        buf = six.BytesIO()
-        buf.write(imgbuf)
-        buf.seek(0)
-        img = Image.open(buf).convert('RGB')
-        target = unpacked[1]
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return img, target
-
-    def __len__(self):
-        return self.length
-
-    def __repr__(self):
-        return self.__class__.__name__ + ' (' + self.db_path + ')'
-
 def raw_reader(path):
     with open(path, 'rb') as f:
         bin_data = f.read()
@@ -165,7 +118,7 @@ if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--folder", type=str)
-    parser.add_argument('-s', '--split', type=str, default="val")
+    parser.add_argument('-s', '--split', type=str, default="train")
     parser.add_argument('--out', type=str, default=".")
     parser.add_argument('-p', '--procs', type=int, default=0)
 
